f8aca10364e483c49cb0b36958b6ff428c697b98
Kevin-abhi <mallabhishekpratap88@gmail.com>
2024-05-12 19:23:21 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-12 bg-primary">
+      <div className="container mx-auto">
+        <p className="text-center text-white">
+          Copyright &copy; Abhishek Pratap 🤍 2024. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
